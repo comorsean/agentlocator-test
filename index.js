58c1949ef2d6d8121fce03535cad0094c94ef93b
sean comor <seancomor@gmail.com>
2022-08-02 01:55:36 +0200

    footer and hero section left

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
+    $(".hamburger").on( "click", function() {
+        $(".hamburger").toggleClass("hamburger--collapse");
+        $(".hamburger").toggleClass("is-active");
+    });
     $('.communities').owlCarousel({
         items: 3,
         loop: true,
-        margin: 20,
-        nav: true,
-        dots: false,
         navText : ["<i class='fa-solid fa-chevron-left'></i>","<i class='fa-solid fa-chevron-right'></i>"],
         responsive: {
             0:{
-                items: 1.5
+                items: 1.2,
+                margin: 10,
+                dots: true,
+                nav: false,
             },
             600:{
                 items: 2.5
             },
             1000:{
-                items: 1
+                items: 1,
+                margin: 20,
+                dots: false,
+                nav: true,
             }
         }
-    })
-})
+    });
+});
